57e9c78d4f15e43829e8f15c55675989d1a2f35f
Alex Zemskov <zemskv@yandex.ru>
2021-10-23 22:21:52 +0300

    JS Commit

---
+++ js/alert.js
@@ -0,0 +1,12 @@
+let value = "";
+let YesNo;
+value = prompt("Введите Ваше имя",0);
+YesNo =  confirm(`Ваше имя ${value}`);
+if (YesNo) {
+    alert('Имя введено верно!');    
+} else {
+    alert(`Имя введено неверно!`);
+}
+
+
+
