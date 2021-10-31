35b1b2132e4a3cf3f95d41f5b5eb94e0db0e5248
Alex Zemskov <zemskv@yandex.ru>
2021-10-31 16:33:39 +0300

    first commit

--- js/alert.js
+++ js/alert.js
@@ -1,12 +1,94 @@
-let value = "";
-let YesNo;
-value = prompt("Введите Ваше имя",0);
-YesNo =  confirm(`Ваше имя ${value}`);
-if (YesNo) {
-    alert('Имя введено верно!');    
-} else {
-    alert(`Имя введено неверно!`);
+
+function checkZero(value){
+    return value==0? true : false; // value==0 || false;
 }
-// для проверки git
-let a = 49;
+function strToNum(value){
+    return Number(value);
+}
+function checkNumber(value){
+    if (value == NaN) {
+        return false;
+    } else {
+        return true;
+    }
+    //return (value == NaN)? false : true;
+}
+function checkAction(value){
+    if (value=="sum" || value=="multi" || value=="subt"||value=="div"||value=="rem"||value=="pow") {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function showMessageErrNum(value){
+    if (!checkNumber(value)) {
+        return confirm("Error Ошибочный ввод! Повторить?");
+    }   
+};
+function showMessageErrAction(valueAction){
+    if (!checkAction(valueAction)) {
+        return confirm("unknown operation! Ошибочный ввод! Повторить?");
+    }
+};
+
+function sum(a, b){
+    return a + b;
+};
+function multi(a, b){
+    return a * b;
+};
+function subt(a, b){
+    return a - b;
+};
+function div(a, b){
+    if (checkZero(b)) {
+        alert("Делить на 0 нельзя!")
+    }else{
+        return a / b;
+    }
+    
+};
+function rem(a, b){
+    if (checkZero(b)) {
+        alert("Делить на 0 нельзя!")
+    }else{
+        return a % b;
+    }
+};
+function pow(a, b){
+    return a ** b;
+};
+let a = 0, b = 0, result = 0, valueAction = "",flContinue;
+do {
+    a = strToNum(prompt('Введите число а',"1"));    
+} while (showMessageErrNum(a));
+if (checkNumber(a)) {
+    do {
+        b = strToNum(prompt('Введите число b','1'));    
+    } while (showMessageErrNum(b));
+    if (checkNumber(b)) {
+        do {
+            valueAction = prompt('Ведите одно математическое действие: sum, multi, subt, div, rem, pow', 'sum');    
+        } while (showMessageErrAction(valueAction)); 
+        if (checkAction(valueAction)) {
+            if (valueAction=="sum") {
+                result = sum(a, b);
+            }else if(valueAction=="multi") {
+                result = multi(a, b);
+            }else if(valueAction=="subt") {
+                result = subt(a, b);
+            }else if(valueAction=="div") {
+                result = div(a, b);
+            }else if(valueAction=="rem") {
+                result = rem(a, b);
+            }else if(valueAction=="pow") {
+                result = pow(a, b);
+            }
+            alert(`Результат ${result}`)
+        }    
+    }
+     
+}
+ 
 
